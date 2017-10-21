f1c59482829468e74d9103644a13f83b49aa6659
CrossR <sc13rjc@leeds.ac.uk>
2017-10-21 14:50:17 +0100

    Added basic interface for DB.

--- main.js
+++ main.js
@@ -9,11 +9,60 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * from Question', function(err, rows, fields) {
-  if (!err)
-    console.log('The Response is: ', rows);
-  else
-    console.log('Error while performing Query.');
-});
+function getQuestion(id) {
+
+    connection.query('SELECT * from Question WHERE id = ?', id, function(err, rows) {
+          if (!err) {
+                console.log('The Response is: ', rows);
+                return rows;
+          } else {
+                console.log('Error while performing Query.');
+                throw(err);
+          }});
+}
+
+function getResponse(id) {
+
+    connection.query('SELECT * from Response WHERE id = ?', id, function(err, rows) {
+          if (!err) {
+                console.log('The Response is: ', rows);
+                return rows;
+          } else {
+                console.log('Error while performing Query.');
+                throw(err);
+          }});
+}
+
+function setQuestion(id) {
+
+    var values = {
+        content: content,
+        QuestionId: id
+    }
+
+    connection.query('INSERT INTO Response VALUES ?', values, function(error) {
+        if (error) {
+            console.log(error.message);
+        } else {
+            console.log('success');
+        }
+    });
+}
+
+function setResponse(id, content) {
+
+    var values = {
+        content: content,
+        QuestionId: id
+    }
+
+    connection.query('INSERT INTO Response VALUES ?', values, function(error) {
+        if (error) {
+            console.log(error.message);
+        } else {
+            console.log('success');
+        }
+    });
+}
 
 connection.end();
