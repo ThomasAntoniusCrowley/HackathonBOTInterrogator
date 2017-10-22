2e2715114c6743afc15903f1c9261007d4cb3441
Peter Cake <peter.g.hristov@gmail.com>
2017-10-22 02:17:27 +0100

    Updated the Website.

--- website/index.js
+++ website/index.js
@@ -1,11 +1,49 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url')
-const rn = require('random-number');
+const Cleverbot = require('cleverbot');
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
 const hostname = '127.0.0.1';
 const port = 8000;
 
+let clev = new Cleverbot({
+      key: 'CC520l1ZK6O1QROHPmTHO1JWtQw' // Can be obtained at http://cleverbot.com/api 
+});
+
+var parameters = {
+
+    'features': 
+    {
+        'categories': {},
+        'concepts': {},
+        'emotion': {},
+        'entities': 
+        {
+            'emotion': true,
+            'sentiment': true,
+            //'limit': 2
+        },
+        'keywords': 
+        {
+            'emotion': true,
+            'sentiment': true,
+            //'limit': 2
+        },
+        'sentiment': {},
+        'relations': {},
+        'semantic_roles': {
+            'entities': true,
+            'keywords': true,
+        }
+    }
+}
+
+var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+    'username': '911c9986-82b1-41d8-a1c1-4a76d0b00cbb',
+    'password': '7ijwXmKEIXSw',
+    'version_date': '2017-02-27'
+});
 
 const server = http.createServer((req, res) => {
 
@@ -15,12 +53,29 @@ const server = http.createServer((req, res) => {
 
     if (path.pathname == '/api') 
     {
-        res.setHeader('Content-Type', 'application/json');
-        var output = {
-            number: rn()
-        };
+        var message = path.query.message;
 
-        res.end(JSON.stringify(output));
+        clev.query(message).then(function (response) { 
+
+            parameters.text = response.output;
+
+            natural_language_understanding.analyze(parameters, function(err, responseNlu) {
+
+                if (err)
+                {
+                    //console.log('error:', err);
+
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify({cleverbot: response.output, watson: null}));
+                }
+                else
+                {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify({cleverbot: response.output, watson: responseNlu}));
+                }
+            });
+
+        });
     }
     else
     {
